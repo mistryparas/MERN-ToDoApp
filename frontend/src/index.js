775620bc1aa6b7b7a8f66ca31bcb38d36bf2fd1c
mrunal-modi <mrunal.modi@gmail.com>
2022-08-20 17:05:17 +1000

    Home

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,27 +1,32 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+// import App from "./App";
 import Crud from "./routes/crud";
 import Swagger from "./routes/swagger";
 import Todo from "./routes/todo";
+import Home from "./routes/home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+    <Navbar />
     <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="CRUD" element={<Crud />} />
-        <Route path="Swagger" element={<Swagger />} />
-        <Route path="ToDo" element={<Todo />} />
-        <Route
-          path="*"
-          element={
-            <main style={{ padding: "1rem" }}>
-              <p>There's nothing here!</p>
-            </main>
-          }
-        />
-      </Route>
+      <Route path="/" element={<Home />}/>
+      <Route path="CRUD" element={<Crud />} />
+      <Route path="Swagger" element={<Swagger />} />
+      <Route path="ToDo" element={<Todo />} />
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>There's nothing here!</p>
+          </main>
+        }
+      />
     </Routes>
+    <Outlet />
+    <Footer />
   </BrowserRouter>
 );
---
+++ frontend/src/routes/home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="home container">
+      <div className="row">
+
+        <div className="col-sm-8">
+          <h2>Home</h2>
+        </div>
+
+        <div className="col-sm-4">
+          Content
+        </div>
+
+      </div>
+    </div>
+  );
+}
