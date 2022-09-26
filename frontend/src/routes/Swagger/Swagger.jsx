5cfe66be507e7f658193a00743b94a3e47c9c7e0
mrunal-modi <mrunal.modi@gmail.com>
2022-09-26 21:24:14 +1000

    updated swagger API_URL

--- frontend/src/routes/Swagger/Swagger.jsx
+++ frontend/src/routes/Swagger/Swagger.jsx
@@ -1,12 +1,15 @@
 import "./Swagger.scss";
-const baseURL = process.env.REACT_APP_API_URL;
+import config from "../config";
+// const baseURL = process.env.REACT_APP_API_URL;
+
+
 export default function Swagger() {
   return (
     <iframe
       title="Powerbi - General Overview"
       width="100%"
       height="100%"
-      src={baseURL + "/api-docs"}
+      src={config.API_URL + "/api-docs"}
       frameBorder="0"
       allowFullScreen={true}
       className={`powerbi-dashboard`}
