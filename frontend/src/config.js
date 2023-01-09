a10dd1a58c04ba085a9d6ab0046894111ce764e0
mrunal-modi <mrunal.modi@gmail.com>
2023-01-10 00:24:18 +0530

    update config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,4 @@
 const config = {};
-config["API_URL"] = `${window.location.origin}/api`;
+// config["API_URL"] = `${window.location.origin}/api`;
+config["API_URL"] = "http://localhost:8080";
 export default config;
