6511cbc43c53fb131f8fd75c62fe27348656fd59
mrunal-modi <mrunal.modi@gmail.com>
2023-01-10 23:28:25 +0530

    update config for k8s nodeport

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,5 @@
 const config = {};
 // config["API_URL"] = `${window.location.origin}/api`;
-config["API_URL"] = "http://192.168.217.132:31815/api";
+// config["API_URL"] = "http://localhost:8080";
+config["API_URL"] = "http://192.168.217.132:32224/api";
 export default config;
