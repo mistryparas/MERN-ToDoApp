b158be4e8dabc7d954de1a17cbf890c12422b41c
mrunal-modi <mrunal.modi@gmail.com>
2023-02-20 19:24:41 +1100

    mm

--- k8s/setup/k8s-app-deployment.sh
+++ k8s/setup/k8s-app-deployment.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-### Ingress
-
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/baremetal/deploy.yaml
-
-
 ### Create
-
 kubectl create -f database-pv.yaml
 kubectl create -f database-pvc.yaml
 kubectl create -f database-deployment.yaml
@@ -20,7 +14,6 @@ kubectl create -f frontend-deployment.yaml
 kubectl create -f frontend-service.yaml
 
 ### Apply
-
 kubectl apply -f database-pv.yaml
 kubectl apply -f database-pvc.yaml
 kubectl apply -f database-deployment.yaml
@@ -33,3 +26,13 @@ kubectl apply -f backend-service.yaml
 kubectl apply -f frontend-deployment.yaml
 kubectl apply -f frontend-service.yaml
 
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.44.0/deploy/static/provider/cloud/deploy.yaml
+# cd kubernetes-ingress/deployments
+# kubectl apply -f common/ns-and-sa.yaml
+# kubectl apply -f common/nginx-config.yaml
+# kubectl apply -f rbac/rbac.yaml
+# kubectl apply -f daemon-set/nginx-ingress.yaml
+# kubectl get pods --namespace=nginx-ingress
+# kubectl describe -n nginx-ingress pod <ingress-pod>
+# kubectl get pods --all-namespaces -l app.kubernetes.io/name=ingress-nginx
+# kubectl delete all --all -n nginx-ingress
