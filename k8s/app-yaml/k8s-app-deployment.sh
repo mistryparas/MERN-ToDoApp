6508177b77855e31f343c2e7f9e8ccbe6db054d4
mrunal-modi <mrunal.modi@gmail.com>
2023-02-21 10:47:17 +1100

    k8s

--- k8s/app-yaml/k8s-app-deployment.sh
+++ k8s/app-yaml/k8s-app-deployment.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+### Docker Login to hub.docker.com so can pull your app images
+docker login
+
 ### Create
 kubectl create -f database-pv.yaml
 kubectl create -f database-pvc.yaml
@@ -25,14 +28,3 @@ kubectl apply -f backend-service.yaml
 
 kubectl apply -f frontend-deployment.yaml
 kubectl apply -f frontend-service.yaml
-
-# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.44.0/deploy/static/provider/cloud/deploy.yaml
-# cd kubernetes-ingress/deployments
-# kubectl apply -f common/ns-and-sa.yaml
-# kubectl apply -f common/nginx-config.yaml
-# kubectl apply -f rbac/rbac.yaml
-# kubectl apply -f daemon-set/nginx-ingress.yaml
-# kubectl get pods --namespace=nginx-ingress
-# kubectl describe -n nginx-ingress pod <ingress-pod>
-# kubectl get pods --all-namespaces -l app.kubernetes.io/name=ingress-nginx
-# kubectl delete all --all -n nginx-ingress
