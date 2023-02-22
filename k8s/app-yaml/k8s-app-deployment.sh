5d4eb3ada69622e2093457b1da21cd2e0eb2a95b
mrunal-modi <mrunal.modi@gmail.com>
2023-02-22 13:31:48 +1100

    mm

--- k8s/app-yaml/k8s-app-deployment.sh
+++ k8s/app-yaml/k8s-app-deployment.sh
@@ -3,9 +3,11 @@
 ### Docker Login to hub.docker.com so can pull your app images
 docker login
 
+cd frontend; make build
 docker tag frontend:latest mmodi/frontend:latest
 docker push mmodi/frontend:latest
 
+cd backend; make build
 docker tag backend:latest mmodi/backend:latest
 docker push mmodi/backend:latest
 
@@ -19,6 +21,7 @@ kubectl create -f backend-env-configmap.yaml
 kubectl create -f backend-deployment.yaml
 kubectl create -f backend-service.yaml
 
+kubectl create -f frontend-env-configmap.yaml
 kubectl create -f frontend-deployment.yaml
 kubectl create -f frontend-service.yaml
 
